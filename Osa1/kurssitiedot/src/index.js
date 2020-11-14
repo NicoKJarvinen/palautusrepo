f29d69c80fb3f0c33b50cc302e4bb247cd0f2a18
Nico Järvinen <nico.k.jarvinen@gmail.com>
2020-11-14 17:33:06 +0200

    Tehtävä 1.5

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -4,29 +4,31 @@ import ReactDOM from 'react-dom'
 const App = () => {
 
  
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
-  const total = <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  const course = 'Half Stack application development'
+  const total = <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
       <Total total={total} />
     </div>
   )
